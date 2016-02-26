9bceea3b1e37f3d412676cad2d9ea8e33e759ed7
adriankierman <adriankierman@hotmail.com>
2016-02-26 12:12:09 -0500

    Added setup for regex based parsing

--- test/envs-quoted.test.js
+++ test/envs-quoted.test.js
@@ -5,9 +5,12 @@ var parsedHash = envs.keyValue(
   '#commented heading. \n' +
   'key = "quoted#hash" \n' +
   'key2 = stripped#comment \n' +
-  'key3 = base64=== \n'
+  'key3 = base64=== \n' + ''
+  //'key4 = "quoted \\n newline" \n'
 )
 
 assert.equal(parsedHash['key'], 'quoted#hash')
 assert.equal(parsedHash['key2'], 'stripped')
 assert.equal(parsedHash['key3'], 'base64===')
+//assert.equal(parsedHash['key4'].replace(/\\n/g, '\n'), 'quoted \n newline')
+//console.log('--------------------------------------->' + parsedHash['key4'].replace(/\\n/g, '\n') )
